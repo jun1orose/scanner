2f19a4e20bc5000ea8f841122ef920c704fe9eb1
Evgene <kinddarose@gmail.com>
2019-09-01 12:06:07 +0300

    Implement custom tree node

---
+++ src/core/CustomTreeNode.java
@@ -0,0 +1,13 @@
+package core;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.nio.file.Path;
+
+class CustomTreeNode extends DefaultMutableTreeNode {
+
+    private Path filePath;
+
+    CustomTreeNode(Path filePath) {
+        super(filePath.getFileName().toString());
+    }
+}
