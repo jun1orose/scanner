e7c8a56869070273a419455bd8f82eda13fef663
Evgene <kinddarose@gmail.com>
2019-09-02 00:06:24 +0300

    Add search panel for search forms

---
+++ src/gui/SearchPanel.java
@@ -0,0 +1,15 @@
+package gui;
+
+import core.Core;
+
+import javax.swing.*;
+
+public class SearchPanel extends JPanel {
+
+    Core core;
+
+    SearchPanel(Core core) {
+        this.core = core;
+    }
+
+}
