9bc67a872545a88f61c5e86e47f73952b1455c83
Evgene <kinddarose@gmail.com>
2019-09-01 22:07:29 +0300

    Implement Tree representation

--- src/gui/FileTree.java
+++ src/gui/FileTree.java
@@ -1,4 +1,54 @@
 package gui;
 
-public class FileTree {
+import core.Core;
+import core.CustomTreeLeaf;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+class FileTree extends JScrollPane {
+
+    private Core core;
+    private JTree fileTree;
+
+    FileTree(Core core) {
+        super();
+
+        this.core = core;
+        fileTree = new JTree(core.getTreeModel());
+        addLoadingFileByDoubleClick();
+
+        setViewportView(fileTree);
+        setOpaque(true);
+
+    }
+
+    private void addLoadingFileByDoubleClick() {
+        fileTree.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+                int selectedRow = fileTree.getRowForLocation(e.getX(), e.getY());
+                TreePath selectedPath = fileTree.getPathForLocation(e.getX(), e.getY());
+
+                if (selectedRow != -1) {
+                    DefaultMutableTreeNode node = null;
+
+                    if (selectedPath != null) {
+                        node = (DefaultMutableTreeNode)selectedPath.getLastPathComponent();
+
+                        if (node.isLeaf()) {
+                            if (e.getClickCount() == 2) {
+                                core.addNewTab((CustomTreeLeaf)node);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+
 }
