36f5311c06a4a9b7c2b23fefc05b7075842cb778
Evgene <kinddarose@gmail.com>
2019-09-03 13:17:00 +0300

    Big refatoring to scroll properly

--- src/gui/TabTextArea.java
+++ src/gui/TabTextArea.java
@@ -1,21 +1,100 @@
 package gui;
 
-import javax.swing.*;
-import javax.swing.text.PlainDocument;
+import core.FileInfo;
+import core.SearchEngine;
 
-public class TabTextArea extends JScrollPane {
+import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import java.awt.*;
+import java.awt.event.AdjustmentListener;
+
+class TabTextArea extends JScrollPane {
 
     private final JTextArea textArea = new JTextArea();
-    private final PlainDocument doc = new PlainDocument();
+    private AdjustmentListener listener;
 
-    TabTextArea() {
+    private final FileInfo fileInfo;
+    private final SearchEngine searchEngine;
+
+    TabTextArea(FileInfo fileInfo, SearchEngine searchEngine) {
         super();
 
-        this.textArea.setLineWrap(true);
-        this.textArea.setWrapStyleWord(true);
-        this.textArea.setDocument(doc);
+        this.fileInfo = fileInfo;
+        this.searchEngine = searchEngine;
 
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setDocument(this.fileInfo.getDoc());
+        setViewportView(textArea);
 
-        this.setViewportView(this.textArea);
+        setLazyFileLoading();
+        activateListener();
+    }
+
+    void toNextMatch() {
+        disableListener();
+        long next = searchEngine.getNextMatch();
+        goToAndSelect(next);
+    }
+
+    void toPrevMatch() {
+        disableListener();
+        long next = searchEngine.getPrevMatch();
+        goToAndSelect(next);
+    }
+
+    private void goToAndSelect(long match) {
+        int actualPos = fileInfo.rollToMatchAndGetActualPos(match);
+
+        try {
+            textArea.requestFocusInWindow();
+            Rectangle viewRect = textArea.modelToView(actualPos);
+            textArea.scrollRectToVisible(viewRect);
+            textArea.setCaretPosition(actualPos + searchEngine.getPatternLength());
+            textArea.moveCaretPosition(actualPos);
+
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        } finally {
+            activateListener();
+        }
+    }
+
+    private void disableListener() {
+        getVerticalScrollBar().removeAdjustmentListener(listener);
+    }
+
+    private void activateListener() {
+        SwingUtilities.invokeLater(() -> getVerticalScrollBar().addAdjustmentListener(listener));
+    }
+
+    private void setLazyFileLoading() {
+        JScrollBar scroll = getVerticalScrollBar();
+        BoundedRangeModel model = scroll.getModel();
+
+        listener = (event) -> {
+
+            if (!fileInfo.isLoading()) {
+
+                int bound = model.getMaximum() / 10;
+
+                if (model.getValue() - model.getMinimum() <= bound) {
+                    fileInfo.loadUpperChunk();
+                    return;
+                }
+
+                if (model.getMaximum() - (model.getValue() + model.getExtent()) <= bound) {
+                    fileInfo.loadLowerChunk();
+                }
+            }
+        };
+    }
+
+    void stopSearch() {
+        searchEngine.stopSearch();
+    }
+
+    void startSearch() {
+        searchEngine.fullSearch();
     }
 }
